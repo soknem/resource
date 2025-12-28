5fa3e200ce856446be42f5517e1d3b22e519e560
soknem <sokname56@gmail.com>
2025-12-28 15:17:25 +0700

    Auto commit

--- src/main/java/com/setec/resource/config/CorsConfig.java
+++ src/main/java/com/setec/resource/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080","http://localhost:3000","http://34.80.194.243","https://supersurvey.live")
-                //.allowedOrigins("https://supersurvey.live")
+                .allowedOrigins("https://superfile.supersurvey.live")
                 .allowedHeaders("*")
                 .exposedHeaders("Set-Cookie")
                 .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS")
