1121ef92ed7b5059d6a21494e4cb58edb668d41d
soknem <sokname56@gmail.com>
2026-01-26 01:42:36 +0700

    change resize preset

--- src/main/java/com/setec/resource/domain/ResizePreset.java
+++ src/main/java/com/setec/resource/domain/ResizePreset.java
@@ -9,10 +9,11 @@ import lombok.Getter;
 public enum ResizePreset {
     CUSTOM(-1,-1),
     ORIGINAL(0, 0),
-    CARD(400, 400),
-    BACKGROUND(1920, 1080),
-    THUMBNAIL(150, 150);
-
+    SD(720, 480),
+    HD(1280, 720),
+    FHD(1920, 1080),
+    QHD(2560, 1440),
+    UHD(4096, 2160);
 
     private final int width;
     private final int height;
